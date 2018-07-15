d9c2bb2e3fab202f27e2c3bd9d28cbbd711a7fc7
Lucas Garron <code@garron.net>
2018-07-15 16:27:12 -0700

    Split `SiGNMove` definitions from other `AlgPart` definitions.

---
+++ src/algorithm/index.ts
@@ -0,0 +1,22 @@
+export {
+  AlgPart,
+  Unit,
+  UnitWithAmount,
+  BaseMove,
+  Sequence,
+  Group,
+  Commutator,
+  Conjugate,
+  Pause,
+  NewLine,
+  CommentShort,
+  CommentLong
+} from "./alg-part";
+
+export {
+  MoveFamily,
+  SiGNMove,
+  BareSiGNMove,
+  LayerSiGNMove,
+  RangeSiGNMove
+} from "./sign-move";
