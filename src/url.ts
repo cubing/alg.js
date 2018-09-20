7f9249fc7ba4d6efd559b145e1e6e20b0252aad0
Lucas Garron <code@garron.net>
2018-09-19 21:50:13 -0700

    Add title parameter to alg.cubing.net URL function.

--- src/url.ts
+++ src/url.ts
@@ -30,6 +30,7 @@ export function getAlgURLParam(name: string): Sequence {
 export type AlgCubingNetOptions = {
   alg?: Sequence;
   setup?: Sequence;
+  title?: string;
   puzzle?: "1x1x1" | "2x2x2" | "3x3x3" | "4x4x4" | "5x5x5" | "6x6x6" | "7x7x7" | "8x8x8" | "9x9x9" | "10x10x10" | "11x11x11" | "12x12x12" | "13x13x13" | "14x14x14" | "16x16x16" | "17x17x17";
   stage?: "full" | "cross" | "F2L" | "LL" | "OLL" | "PLL" | "CLS" | "ELS" | "L6E" | "CMLL" | "WV" | "ZBLL" | "void";
   view?: "editor" | "playback" | "fullscreen"
@@ -42,10 +43,13 @@ export function algCubingNetLink(options: AlgCubingNetOptions) {
   if (!options.alg) {
     throw ("An alg parameter is required.");
   }
-  url.searchParams.set("alg", serializeURLParam(options.alg))
+  url.searchParams.set("alg", serializeURLParam(options.alg));
 
   if (options.setup) {
-    url.searchParams.set("setup", serializeURLParam(options.setup))
+    url.searchParams.set("setup", serializeURLParam(options.setup));
+  }
+  if (options.title) {
+    url.searchParams.set("title", options.title);
   }
   if (options.puzzle) {
     if (["1x1x1", "2x2x2", "3x3x3", "4x4x4", "5x5x5", "6x6x6", "7x7x7", "8x8x8", "9x9x9", "10x10x10", "11x11x11", "12x12x12", "13x13x13", "14x14x14", "16x16x16", "17x17x17"].indexOf(options.puzzle) === -1) {
